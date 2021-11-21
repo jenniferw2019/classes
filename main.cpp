2cfa2f0cabd3cfd4480ff487244724fbeff7789d
jenniferw2019 <jenniferwpdx2018@gmail.com>
2021-11-20 23:42:15 -0800

    version four

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 void addMedia(Media* pMedia, vector<Media*>* pVector);
 void printMedia(vector<Media*> pMediaPrint);
 void searchMedia(vector<Media*> pMediaSearch);
+void deleteMedia(vector<Media*>* pMediaDelete);
 
 int main()
 {
@@ -22,7 +23,7 @@ int main()
 
   while (runProgram == true)
     {
-      cout << "Type ADD or SEARCH or QUIT" << endl;
+      cout << "Type ADD or SEARCH or DELETE or QUIT" << endl;
       cin.get(input,10);
       cin.get();
 
@@ -52,8 +53,13 @@ int main()
 	    }
 	  */
 
-
 	}
+
+      if (strcmp(input, "DELETE") == 0)
+	{
+	  deleteMedia(ptrVecMedia);
+	}
+      
       if (strcmp(input, "QUIT") == 0)
 	{
 	  printMedia(vecMediaList);
@@ -131,6 +137,121 @@ void searchMedia(vector<Media*> pVecMedia)
 	    }
 	}
     }
+}
 
+void deleteMedia(vector<Media*>* pDeleteMedia)
+{
 
+  char inDeleteTitle[100];
+  int inDeleteYear = 0;
+  char search;
+  char yesno;
+  int count = 0;
+  int index = 0;
+
+  cout << "Delete by Title or Year? Enter 'T' for Title or 'Y' for Year" << endl;
+  cin >> search;
+  cin.get();
+  //cout << search << endl;
+  
+  
+  if (search == 'T')
+    {
+      cout << "Enter Title" << endl;
+      cin.get(inDeleteTitle,100);
+      cin.get();
+      // cout << inDeleteTitle;
+      
+      
+      for (vector<Media*>::iterator it = pDeleteMedia->begin(); it != pDeleteMedia->end(); it++)
+	{
+	  if (strcmp(inDeleteTitle, (*it)->getTitle()) == 0)
+	    {
+	      (*it)->display();
+	      count++;
+	    }
+	}
+
+      cout << "Are you sure you want to delete these? Type 'y' for yes or 'n' for no" << endl;
+      cin >> yesno;
+      cin.get();
+
+      if (yesno == 'y')
+	{
+	  
+	  while(count != 0)
+	    {
+	      for (vector<Media*>:: iterator it = pDeleteMedia->begin(); it != pDeleteMedia->end(); it++)
+		{
+		  
+		  if (strcmp(inDeleteTitle, (*it)->getTitle()) == 0)
+		    {
+		      delete *(pDeleteMedia->begin()+ index);
+		      pDeleteMedia->erase(pDeleteMedia->begin()+ index);
+		      count = count - 1;
+		      break;
+		    }
+		  else
+		    {
+		      index++;
+		    }
+		}
+	    }
+	  
+	}
+      else
+	{
+	  cout << "Ok, no delete" << endl;
+	} 
+    }
+  
+
+  
+ else if (search == 'Y')
+    {
+      cout << "Enter Year" << endl;
+      cin >> inDeleteYear;
+      cin.get();
+
+      for (vector<Media*>::iterator it = pDeleteMedia->begin(); it != pDeleteMedia->end(); it++)
+	{
+	  if (inDeleteYear == (*it)->getYear())
+	    {
+	      (*it)->display();
+	      count++;
+	    }
+	}
+
+      cout << "Are you sure you want to delete these? Type 'y' for yes or 'n' for no" << endl;
+      cin >> yesno;
+      cin.get();
+  
+      if (yesno == 'y')
+	{ 
+	  while(count != 0)
+	    {
+	      for (vector<Media*>:: iterator it = pDeleteMedia->begin(); it != pDeleteMedia->end(); it++)
+		{
+
+		  if (inDeleteYear == (*it)->getYear())
+		    {
+		      delete *(pDeleteMedia->begin()+ index);
+		      pDeleteMedia->erase(pDeleteMedia->begin()+ index);
+		      count = count - 1;
+		      break;
+		    }
+		  else
+		    {
+		      index++;
+		    }
+		}
+	    }
+	  
+	}
+      else
+	{
+	  cout << "Ok, no delete" << endl;
+	}
+
+    }
 }
